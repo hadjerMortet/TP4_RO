e4ac5b260ef0b800171969dabfdd72eb579d5fec
hadjerMortet <72406358+hadjerMortet@users.noreply.github.com>
2022-05-31 20:15:02 +0200

    Update EdgeWeightedDirectedCycle.java

--- TP4_RO/src/Arbitrage/bellmanford/princeton/modify/EdgeWeightedDirectedCycle.java
+++ TP4_RO/src/Arbitrage/bellmanford/princeton/modify/EdgeWeightedDirectedCycle.java
@@ -1,10 +1,10 @@
-package az.mm.arbitrage.bellmanford.princeton.modify;
+package arbitrage.bellmanford.princeton.modify;
 
 public class EdgeWeightedDirectedCycle {
-    private boolean[] marked;             // marked[v] = has vertex v been marked?
-    private DirectedEdge[] edgeTo;        // edgeTo[v] = previous edge on path to v
-    private boolean[] onStack;            // onStack[v] = is vertex on the stack?
-    private Stack<DirectedEdge> cycle;    // directed cycle (or null if no such cycle)
+    private boolean[] marked;             
+    private DirectedEdge[] edgeTo;        
+    private boolean[] onStack;            
+    private Stack<DirectedEdge> cycle;    
 
     public EdgeWeightedDirectedCycle(EdgeWeightedDigraph G) {
         marked  = new boolean[G.V()];
@@ -14,23 +14,22 @@ public class EdgeWeightedDirectedCycle {
             if (!marked[v]) dfs(G, v);
     }
 
-    // check that algorithm computes either the topological order or finds a directed cycle
+    
     private void dfs(EdgeWeightedDigraph G, int v) {
         onStack[v] = true;
         marked[v] = true;
         for (DirectedEdge e : G.adj(v)) {
             int w = e.to();
 
-            // short circuit if directed cycle found
+           
             if (cycle != null) return;
 
-            //found new vertex, so recur
             else if (!marked[w]) {
                 edgeTo[w] = e;
                 dfs(G, w);
             }
 
-            // trace back directed cycle
+            
             else if (onStack[w]) {
                 cycle = new Stack<DirectedEdge>();
                 while (e.from() != w) {
