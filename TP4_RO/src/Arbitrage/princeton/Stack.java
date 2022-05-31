a47511140e900cb6d316a8184813eca6708ff35a
hadjerMortet <72406358+hadjerMortet@users.noreply.github.com>
2022-05-31 18:34:36 +0200

    Update Stack.java

--- TP4_RO/src/Arbitrage/princeton/Stack.java
+++ TP4_RO/src/Arbitrage/princeton/Stack.java
@@ -1,23 +1,4 @@
-package az.mm.arbitrage.princeton;
-
-/*************************************************************************
- *  Compilation:  javac Stack.java
- *  Execution:    java Stack < input.txt
- *
- *  A generic stack, implemented using a singly-linked list.
- *  Each stack element is of type Item.
- *
- *  This version uses a static nested class Node (to save 8 bytes per
- *  Node), whereas the version in the textbook uses a non-static nested
- *  class (for simplicity).
- *  
- *  % more tobe.txt 
- *  to be or not to - be - - that - - - is
- *
- *  % java Stack < tobe.txt
- *  to be not that or be (2 left on stack)
- *
- *************************************************************************/
+package arbitrage.princeton;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -26,62 +7,34 @@ import edu.princeton.cs.introcs.StdIn;
 import edu.princeton.cs.introcs.StdOut;
 
 
-/**
- *  The <tt>Stack</tt> class represents a last-in-first-out (LIFO) stack of generic items.
- *  It supports the usual <em>push</em> and <em>pop</em> operations, along with methods
- *  for peeking at the top item, testing if the stack is empty, and iterating through
- *  the items in LIFO order.
- *  <p>
- *  This implementation uses a singly-linked list with a static nested class for
- *  linked-list nodes. See {@link LinkedStack} for the version from the
- *  textbook that uses a non-static nested class.
- *  The <em>push</em>, <em>pop</em>, <em>peek</em>, <em>size</em>, and <em>is-empty</em>
- *  operations all take constant time in the worst case.
- *  <p>
- *  For additional documentation, see <a href="/algs4/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- */
-public class Stack<Item> implements Iterable<Item> {
-    private int N;                // size of the stack
-    private Node<Item> first;     // top of stack
 
-    // helper linked list class
+public class Stack<Item> implements Iterable<Item> {
+    private int N;                
+    private Node<Item> first;     
+
+   
     private static class Node<Item> {
         private Item item;
         private Node<Item> next;
     }
 
-    /**
-     * Initializes an empty stack.
-     */
+   
     public Stack() {
         first = null;
         N = 0;
     }
 
-    /**
-     * Is this stack empty?
-     * @return true if this stack is empty; false otherwise
-     */
+   
     public boolean isEmpty() {
         return first == null;
     }
 
-    /**
-     * Returns the number of items in the stack.
-     * @return the number of items in the stack
-     */
+   
     public int size() {
         return N;
     }
 
-    /**
-     * Adds the item to this stack.
-     * @param item the item to add
-     */
+    
     public void push(Item item) {
         Node<Item> oldfirst = first;
         first = new Node<Item>();
@@ -90,34 +43,23 @@ public class Stack<Item> implements Iterable<Item> {
         N++;
     }
 
-    /**
-     * Removes and returns the item most recently added to this stack.
-     * @return the item most recently added
-     * @throws java.util.NoSuchElementException if this stack is empty
-     */
+   
     public Item pop() {
         if (isEmpty()) throw new NoSuchElementException("Stack underflow");
-        Item item = first.item;        // save item to return
-        first = first.next;            // delete first node
+        Item item = first.item;        
+        first = first.next;            
         N--;
-        return item;                   // return the saved item
+        return item;                  
     }
 
 
-    /**
-     * Returns (but does not remove) the item most recently added to this stack.
-     * @return the item most recently added to this stack
-     * @throws java.util.NoSuchElementException if this stack is empty
-     */
+   
     public Item peek() {
         if (isEmpty()) throw new NoSuchElementException("Stack underflow");
         return first.item;
     }
 
-    /**
-     * Returns a string representation of this stack.
-     * @return the sequence of items in the stack in LIFO order, separated by spaces
-     */
+   
     @Override
     public String toString() {
         StringBuilder s = new StringBuilder();
@@ -127,15 +69,11 @@ public class Stack<Item> implements Iterable<Item> {
     }
        
 
-    /**
-     * Returns an iterator to this stack that iterates through the items in LIFO order.
-     * @return an iterator to this stack that iterates through the items in LIFO order.
-     */
     public Iterator<Item> iterator() {
         return new ListIterator<Item>(first);
     }
 
-    // an iterator, doesn't implement remove() since it's optional
+    
     private class ListIterator<Item> implements Iterator<Item> {
         private Node<Item> current;
 
@@ -154,9 +92,7 @@ public class Stack<Item> implements Iterable<Item> {
     }
 
 
-    /**
-     * Unit tests the <tt>Stack</tt> data type.
-     */
+   
     public static void main(String[] args) {
         Stack<String> s = new Stack<String>();
         while (!StdIn.isEmpty()) {
