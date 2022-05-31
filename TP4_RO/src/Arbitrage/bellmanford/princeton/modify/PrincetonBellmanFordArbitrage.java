1cd12fd40bda643511aea5315543416b43fec0a3
hadjerMortet <72406358+hadjerMortet@users.noreply.github.com>
2022-05-31 20:15:54 +0200

    Update PrincetonBellmanFordArbitrage.java

--- TP4_RO/src/Arbitrage/bellmanford/princeton/modify/PrincetonBellmanFordArbitrage.java
+++ TP4_RO/src/Arbitrage/bellmanford/princeton/modify/PrincetonBellmanFordArbitrage.java
@@ -1,16 +1,11 @@
-package az.mm.arbitrage.bellmanford.princeton.modify;
+package arbitrage.bellmanford.princeton.modify;
 
-import az.mm.arbitrage.cache.DataCache;
-import az.mm.arbitrage.factory.*;
-import az.mm.arbitrage.model.OptimalRate;
+import arbitrage.cache.DataCache;
+import arbitrage.factory.*;
+import arbitrage.model.OptimalRate;
 import edu.princeton.cs.introcs.StdOut;
 
-/**
- * Bu paketdeki classlar Princeton numunesinin modifikasiya olunmush formalaridir.
- * Orijinal classlar az.mm.arbitrage.princeton paketindedir.
- *
- * @author MM <mushfiqazeri@gmail.com>
- */
+
 public class PrincetonBellmanFordArbitrage implements Arbitrage {
     
     @Override
@@ -23,12 +18,12 @@ public class PrincetonBellmanFordArbitrage implements Arbitrage {
           for (int v = 0; v < V; v++) 
             for (int w = 0; w < V; w++) {
                 double rate = R[v][w].getValue();
-                if(rate == -1) continue; //no conversion
+                if(rate == -1) continue; 
                 DirectedEdge e = new DirectedEdge(v, w, -Math.log(rate), R[v][w].getBankName());
                 G.addEdge(e);
             }
         
-        // find negative cycle
+        
         BellmanFordSP spt = new BellmanFordSP(G, 0);
         if (spt.hasNegativeCycle()) {
             System.out.println("\nArbitrage:");
